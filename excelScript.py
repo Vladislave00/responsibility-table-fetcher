6e8689e9145ace169beb1d10cf9f53638d4ca266
Vlad Afonin <followerlsp@gmail.com>
2024-12-08 16:15:36 +0300

    fix

--- excelScript.py
+++ excelScript.py
@@ -3,39 +3,6 @@ import openpyxl.styles
 import dbalch
 
 
-# Метод для форматирования границ таблицы
-def set_border(ws, cell_range, need_to_thick, need_to_thick_up, need_to_thick_down):
-    thin = openpyxl.styles.Side(border_style="thin", color="000000")
-    thick = openpyxl.styles.Side(border_style="thick", color="000000")
-    for row in ws[cell_range]:
-        for cell in row:
-            if cell == row[len(row) - 1]:
-                cell.border = openpyxl.styles.Border(top=thin, left=thin, right=thick, bottom=thin)
-                cell.fill = openpyxl.styles.PatternFill(start_color="D9D9D9", end_color="D9D9D9", fill_type="solid")
-            else:
-                cell.border = openpyxl.styles.Border(top=thin, left=thin, right=thin, bottom=thin)
-        if len(need_to_thick) != 0:
-            if row == ws[need_to_thick[0]]:
-                for cell in row:
-                    cell.border = openpyxl.styles.Border(top=thick, left=thick, right=thick, bottom=thick)
-                need_to_thick.pop(0)
-        if len(need_to_thick_up) != 0:
-            if row == ws[need_to_thick_up[0]]:
-                for cell in row:
-                    if cell == row[len(row) - 1]:
-                        cell.border = openpyxl.styles.Border(top=thick, left=thin, right=thick, bottom=thin)
-                    else:
-                        cell.border = openpyxl.styles.Border(top=thick, left=thin, right=thin, bottom=thin)
-                need_to_thick_up.pop(0)
-        if len(need_to_thick_down) != 0:
-            if row == ws[need_to_thick_down[0]]:
-                for cell in row:
-                    if cell == row[len(row) - 1]:
-                        cell.border = openpyxl.styles.Border(top=thin, left=thin, right=thick, bottom=thick)
-                    else:
-                        cell.border = openpyxl.styles.Border(top=thin, left=thin, right=thin, bottom=thick)
-
-                need_to_thick_down.pop(0)
 
 
 # Метод для формирования Excel таблицы
